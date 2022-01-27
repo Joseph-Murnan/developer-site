32681c7ba2878a100e6c5ac9d69ed056e5344985
Joseph-Murnan <josephmurnan@gmail.com>
2022-01-27 18:52:48 +0000

    Dealing with drag ghost and adjusting separator style.

--- components/Slider/Facade/Facade.tsx
+++ components/Slider/Facade/Facade.tsx
@@ -2,13 +2,18 @@ import { Fragment } from 'react';
 import styles from '../Slider.module.css';
 
 const Facade = () => {
+    const onDragStart = (e: React.DragEvent) => {
+        const img = new Image();
+        img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
+        e.dataTransfer.setDragImage(img, 0, 0);
+    }
     return (
         <Fragment>
             <div className="grid">
                 <div className="left"><span>left content</span></div>
                 <div className="right"><span>right content</span></div>
             </div>
-            <div draggable="true" className={styles.separator}>
+            <div draggable="true" onDragStart={e => onDragStart(e)} className={styles.separator}>
                 <div className={styles.indicator}></div>
             </div>
         </Fragment>
