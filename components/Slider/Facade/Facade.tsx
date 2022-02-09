3f1b6bcbb3055fd87cf9b4f351394c7544836068
Joseph-Murnan <josephmurnan@gmail.com>
2022-02-09 19:13:23 +0000

    Creating drag events.

--- components/Slider/Facade/Facade.tsx
+++ components/Slider/Facade/Facade.tsx
@@ -7,13 +7,19 @@ const Facade = () => {
         img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
         e.dataTransfer.setDragImage(img, 0, 0);
     }
+    const onDragEnd = (e: React.DragEvent) => {
+        
+    };
+    const onDrag = (e: React.DragEvent) => {
+        
+    };
     return (
         <Fragment>
             <div className="grid">
                 <div className="left"><span>left content</span></div>
                 <div className="right"><span>right content</span></div>
             </div>
-            <div draggable="true" onDragStart={e => onDragStart(e)} className={styles.separator}>
+            <div draggable="true" onDragStart={e => onDragStart(e)} onDrag={e => onDrag(e)} onDragEnd={e => onDragEnd(e)} className={styles.separator}>
                 <div className={styles.indicator}></div>
             </div>
         </Fragment>
