57858b4848029c9b2d9ff764ab3ae32fe67859c0
Joseph-Murnan <josephmurnan@gmail.com>
2022-04-13 18:56:52 +0100

    Front end for facade component.

--- components/Slider/Facade/Facade.tsx
+++ components/Slider/Facade/Facade.tsx
@@ -1,23 +1,25 @@
-import { Fragment, ReactElement } from 'react';
+import { Fragment, ReactElement, DragEvent } from 'react';
 import styles from '../Slider.module.css';
 
 const Facade = (): ReactElement => {
-    const onDragStart = (e: React.DragEvent) => {
+    const onDragStart = (e: DragEvent) => {
         const img = new Image();
         img.src = 'data:image/gif;base64,R0lGODlhAQABAIAAAAUEBAAAACwAAAAAAQABAAACAkQBADs=';
         e.dataTransfer.setDragImage(img, 0, 0);
     }
-    const onDragEnd = (e: React.DragEvent) => {
+    const onDragEnd = (e: DragEvent) => {
         
     };
-    const onDrag = (e: React.DragEvent) => {
+    const onDrag = (e: DragEvent) => {
         
     };
     return (
         <Fragment>
-            <div className="grid">
-                <div className="left"><span>left content</span></div>
-                <div className="right"><span>right content</span></div>
+            <div className={styles.facadeContainer}>
+                <div className={`${styles.facade} grid`}>
+                    <div className="left"><span>left content</span></div>
+                    <div className="right"><span>right content</span></div>
+                </div>
             </div>
             <div draggable="true" onDragStart={e => onDragStart(e)} onDrag={e => onDrag(e)} onDragEnd={e => onDragEnd(e)} className={styles.separator}>
                 <div className={styles.indicator}></div>
